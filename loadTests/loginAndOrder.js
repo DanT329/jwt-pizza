1d56a281536f69f7bab9501a20c4331c1f716976
DanT329 <140003581+DanT329@users.noreply.github.com>
2024-11-17 13:17:15 -0700

    Create loginAndOrder.js

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,288 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+    Imported_HAR: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 20, duration: '1m' },
+        { target: 20, duration: '3m30s' },
+        { target: 0, duration: '1m' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'imported_HAR',
+    },
+    Imported_HAR1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 20, duration: '1m' },
+        { target: 20, duration: '3m30s' },
+        { target: 0, duration: '1m' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'imported_HAR1',
+    },
+  },
+}
+
+// Scenario: Scenario_1 (executor: ramping-vus)
+
+export function scenario_1() {
+  let response
+
+  // Automatically added sleep
+  sleep(1)
+}
+
+// Scenario: Imported_HAR (executor: ramping-vus)
+
+export function imported_HAR() {
+  let response
+
+  // Automatically added sleep
+  sleep(1)
+}
+
+// Scenario: Imported_HAR1 (executor: ramping-vus)
+
+export function imported_HAR1() {
+  let response;
+  let authToken;
+  let jwt;
+
+  group('page_2 - https://pizza.cs329pizzajwt.click/', function () {
+    response = http.get('https://pizza.cs329pizzajwt.click/', {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        'if-modified-since': 'Thu, 31 Oct 2024 00:14:01 GMT',
+        'if-none-match': '"9aa31b53bddf2422146d7590b3ef783c"',
+        priority: 'u=0, i',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+    })
+    sleep(6.4)
+
+    response = http.put(
+      'https://pizza-service.cs329pizzajwt.click/api/auth',
+      '{"email":"danny@gmail.com","password":"password"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.cs329pizzajwt.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    authToken = response.json('token');
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Login was *not* 200');
+    }
+    sleep(5)
+  
+
+    response = http.options('https://pizza-service.cs329pizzajwt.click/api/auth', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'content-type',
+        'access-control-request-method': 'PUT',
+        origin: 'https://pizza.cs329pizzajwt.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(4.6)
+
+    response = http.get('https://pizza-service.cs329pizzajwt.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        origin: 'https://pizza.cs329pizzajwt.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    response = http.options('https://pizza-service.cs329pizzajwt.click/api/order/menu', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'GET',
+        origin: 'https://pizza.cs329pizzajwt.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    response = http.get('https://pizza-service.cs329pizzajwt.click/api/franchise', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        origin: 'https://pizza.cs329pizzajwt.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    response = http.options('https://pizza-service.cs329pizzajwt.click/api/franchise', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'GET',
+        origin: 'https://pizza.cs329pizzajwt.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(20.3)
+
+    response = http.post(
+      'https://pizza-service.cs329pizzajwt.click/api/order',
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.cs329pizzajwt.click',
+          authorization: `Bearer ${authToken}`,
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    jwt = response.json('jwt');
+
+    if (!check(response, { 'order succeed': (r) => r.status === 200 && jwt })) {
+      console.log(response.body);
+      fail('Order failed');
+    }
+
+    
+
+    response = http.options('https://pizza-service.cs329pizzajwt.click/api/order', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'POST',
+        origin: 'https://pizza.cs329pizzajwt.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(5.6)
+
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      JSON.stringify({ jwt }),
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.cs329pizzajwt.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+        },
+      }
+    )
+
+
+    response = http.options('https://pizza-factory.cs329.click/api/order/verify', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'POST',
+        origin: 'https://pizza.cs329pizzajwt.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+      },
+    })
+  })
+}
